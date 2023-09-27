70cbe368ab0222595daa5d6fd20c5f63b2fa9411
Matheus Andrade <matheuslincon50@gmail.com>
2023-09-27 14:14:44 +1000

    added employers table

--- src/pages/Employers/index.tsx
+++ src/pages/Employers/index.tsx
@@ -1,9 +1,42 @@
 import { Header } from "../../components/Header";
+import { ContractHighlight, EmployersContainer, EmployersTable } from "./styles";
 
 export function Employers() {
   return (
     <h1>
       <Header/>
+      <EmployersContainer>
+        <EmployersTable>
+          <tbody>
+            <tr>
+              <td width="50%">John Smith</td>
+              <td>
+                <ContractHighlight variant="permanent">
+                  Permanent
+                </ContractHighlight>
+              </td>
+              <td>johnsmith@email.com</td>
+              <td>
+                <button>edit</button>
+                <button>delete</button>
+              </td>
+            </tr>
+            <tr>
+              <td width="50%">Matheus Smith</td>
+              <td>
+                <ContractHighlight variant="contract">
+                  Contract
+                </ContractHighlight>
+              </td>
+              <td>matheussmith@email.com</td>
+              <td>
+                <button>edit</button>
+                <button>delete</button>
+              </td>
+            </tr>
+          </tbody>
+        </EmployersTable>
+      </EmployersContainer>
     </h1>
   )
 }
--- src/pages/Employers/styles.ts
+++ src/pages/Employers/styles.ts
@@ -1,5 +1,40 @@
-export function Employers() {
-  return (
-    <h1>Hello</h1>
-  )
+import styled from "styled-components";
+
+export const EmployersContainer = styled.main`
+  width: 100%;
+  max-width: 1120px;
+  margin: 4rem auto;
+  padding: 0 1.5rem;
+`
+
+export const EmployersTable = styled.table`
+  width: 100%;
+  border-collapse: separate;
+  border-spacing: 0 0.5rem;
+
+  td {
+    padding: 1.25rem 2rem;
+    background: ${props => props.theme["gray-700"]};
+    font-size: 1rem;
+    font-style: normal;
+
+
+    &:first-child {
+      border-top-left-radius: 6px;
+      border-bottom-left-radius: 6px;
+    }
+
+    &:last-child {
+      border-top-right-radius: 6px;
+      border-bottom-right-radius: 6px;
+    }
+  }
+`
+
+interface ContractHighlightProps {
+  variant: 'contract' | 'permanent';
 }
+
+export const ContractHighlight = styled.span<ContractHighlightProps>`
+  color: ${props => props.variant === 'permanent' ? props.theme["green-300"] : props.theme.orange};
+`
